8e1545b35ab2477a17d4a9416f3967088fe8e44d
agniadzik <gniadzik@gmail.com>
2015-01-29 09:00:16 +0100

    Upload Workflow Command fix: workflow definition name fixed

--- src/main/java/pl/nask/hsn2/unicorn/commands/UploadWorkflowCommand.java
+++ src/main/java/pl/nask/hsn2/unicorn/commands/UploadWorkflowCommand.java
@@ -1,10 +1,6 @@
 package pl.nask.hsn2.unicorn.commands;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -15,23 +11,24 @@ import pl.nask.hsn2.unicorn.connector.ConnectionException;
 
 public class UploadWorkflowCommand extends BasicRPCCommand {
 	private final static String REQUEST_TYPE = "WorkflowUploadRequest";
-	private String workflowFileName;
+	private String workflowFilePath;
 
 
-	public UploadWorkflowCommand(String queueName, String workflowFileName)
+	public UploadWorkflowCommand(String queueName, String workflowFilePath)
 			throws ConnectionException {
 		super(REQUEST_TYPE, queueName);
-		this.workflowFileName = workflowFileName;
+		this.workflowFilePath = workflowFilePath;
 	}
 
 	@Override
 	protected void buildMessage() {
-		this.message = WorkflowUploadRequest.newBuilder().setName(workflowFileName).setContent(workflowContent()).setOverwrite(true).build().toByteArray();
+		String workflowName = Paths.get(workflowFilePath).getFileName().toString();
+		this.message = WorkflowUploadRequest.newBuilder().setName(workflowName).setContent(workflowContent()).setOverwrite(true).build().toByteArray();
 	}
 
 	private String workflowContent() {
 		try {
-			List<String> lines = Files.readAllLines(Paths.get(workflowFileName), StandardCharsets.UTF_8);
+			List<String> lines = Files.readAllLines(Paths.get(workflowFilePath), StandardCharsets.UTF_8);
 			StringBuilder builder = new StringBuilder();
 			for (String line: lines) {
 				builder.append(line);
